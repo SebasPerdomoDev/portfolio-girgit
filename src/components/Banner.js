b3881cda44439d03261dc294a7904c43e7d75a29
SebasDev <sebastianoliverosxd2002@gmail.com>
2025-05-02 15:41:47 -0500

    Agg colores y imagenes 1.0

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const toRotate = [ "Desarrollo Web", "Plataformas Web", "Software y más" ];
   const period = 2000;
 
   useEffect(() => {
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Bienvenido a GirGit</span>
                 <h1>{`Hi, somos GirGit`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Desarrollo Web", "Plataformas", "Software y mas" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  
                   <button onClick={() => console.log('connect')}>Contactanos <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
